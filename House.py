dca8f7906f426afb4e58c1dec989c424a4da1702
mark odima <89321878+Ojeanm@users.noreply.github.com>
2022-11-19 12:42:55 +0300

    Create House.py

---
+++ House.py
@@ -0,0 +1,30 @@
+import pandas as pd # data processing
+import numpy as np # working with arrays
+import matplotlib.pyplot as plt # visualization
+import seaborn as sb # visualization
+from termcolor import colored as cl # text customization
+
+from sklearn.model_selection import train_test_split # data split
+
+from sklearn.linear_model import LinearRegression # OLS algorithm
+from sklearn.linear_model import Ridge # Ridge algorithm
+from sklearn.linear_model import Lasso # Lasso algorithm
+from sklearn.linear_model import BayesianRidge # Bayesian algorithm
+from sklearn.linear_model import ElasticNet # ElasticNet algorithm
+
+from sklearn.metrics import explained_variance_score as evs # evaluation metric
+from sklearn.metrics import r2_score as r2 # evaluation metric
+
+sb.set_style('whitegrid') # plot style
+plt.rcParams['figure.figsize'] = (20, 10) # plot size
+
+df = pd.read_csv('house.csv')
+
+
+df.head(100)
+
+df.describe()
+
+df.std()
+
+df.max()
